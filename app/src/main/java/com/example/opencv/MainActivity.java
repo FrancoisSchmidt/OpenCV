ebb7388bab10e865d69729767234d7d4f62080e2
Franz57 <francois.schmidt@outlook.com>
2020-01-23 22:18:29 +0100

    Version 2 qui fonctionne

--- app/src/main/java/com/example/opencv/MainActivity.java
+++ app/src/main/java/com/example/opencv/MainActivity.java
@@ -1,205 +1,123 @@
 package com.example.opencv;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
-
 import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
-import android.view.View.OnTouchListener;
-import android.view.SurfaceView;
-import android.widget.Button;
-import android.widget.TextView;
 
-public class MainActivity extends Activity implements OnTouchListener, CvCameraViewListener2 {
-    private static final String  TAG              = "MainActivity";
-    private static final Scalar  FACE_RECT_COLOR  = new Scalar(0,255,0,255);
-    public static final int JAVA_DETECTOR = 0;
 
-    private Mat mRgba;
-    private Mat mGray;
-    private File mCascadeFile;
-    private CascadeClassifier mJavaDetectorFace;
-    boolean check = false;
+public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener {
+    private CameraBridgeViewBase openCvCameraView;
+    private CascadeClassifier cascadeClassifier;
+    private Mat grayscaleImage;
+    private int absoluteFaceSize;
 
-    private int mDetectorType = JAVA_DETECTOR;
-    private String[] mDetectorName;
-
-    private float mRelativeFaceSize = 0.2f;
-    private int mAbsoluteFaceSize = 0;
-
-    private CameraBridgeViewBase mOpenCvCameraView;
-
-    public Button mSpeechbutton;
-    public TextView checkResults;
-
-    double xCenter = -1;
-    double yCenter = -1;
-
-    private BaseLoaderCallback  mLoaderCallback = new BaseLoaderCallback(this) {
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
-            switch (status) {
-                case LoaderCallbackInterface.SUCCESS:
-                {
-                    Log.i(TAG, "OpenCV loaded successfully");
-                    try {
-                        InputStream is = getResources().openRawResource(R.raw.haarcascade_frontalface_alt2);
-                        File cascadeDir = getDir("cascade",Context.MODE_PRIVATE);
-                        mCascadeFile = new File(cascadeDir, "haarcascade_frontalface_alt2.xml");
-                        FileOutputStream os = new FileOutputStream(mCascadeFile);
-
-                        byte[] buffer = new byte[4096];
-                        int bytesRead;
-                        while ((bytesRead = is.read(buffer)) != -1) {
-                            os.write(buffer, 0, bytesRead);
-                        }
-                        is.close();
-                        os.close();
-
-                        mJavaDetectorFace = new CascadeClassifier(mCascadeFile.getAbsolutePath());
-                        if (mJavaDetectorFace.empty()) {
-                            Log.e(TAG, "Failed to load cascade classifier");
-                            mJavaDetectorFace = null;
-                        } else
-                            Log.i(TAG, "Loaded cascade classifier from " + mCascadeFile.getAbsolutePath());
-
-                        cascadeDir.delete();
-                    } catch (IOException e){
-                        e.printStackTrace();
-                        Log.e(TAG,"Failed to load cascade");
-                    }
-                    mOpenCvCameraView.enableFpsMeter();
-                    mOpenCvCameraView.setCameraIndex(0);
-                    mOpenCvCameraView.enableView();
-                    mOpenCvCameraView.setOnTouchListener(MainActivity.this);
-                } break;
-                default:
-                {
-                    super.onManagerConnected(status);
-                } break;
+            if (status == LoaderCallbackInterface.SUCCESS) {
+                initializeOpenCVDependencies();
+            } else {
+                super.onManagerConnected(status);
             }
         }
     };
 
-    public MainActivity() {
-        mDetectorName = new String[2];
-        mDetectorName[JAVA_DETECTOR] = "Java";
-        Log.i(TAG, "Instantiated new " + this.getClass());
+    private void initializeOpenCVDependencies() {
+
+        try {
+            // Copy the resource into a temp file so OpenCV can load it
+            InputStream is = getResources().openRawResource(R.raw.haarcascade_frontalface_alt2);
+            File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+            File mCascadeFile = new File(cascadeDir, "haarcascade_frontalface_alt2.xml");
+            FileOutputStream os = new FileOutputStream(mCascadeFile);
+
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            is.close();
+            os.close();
+
+            // Load the cascade classifier
+            cascadeClassifier = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+        } catch (Exception e) {
+            Log.e("OpenCVActivity", "Error loading cascade", e);
+        }
+
+        // And we are ready to go
+        openCvCameraView.enableView();
     }
 
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        Log.i(TAG, "called onCreate");
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        setContentView(R.layout.activity_main);
-
-        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.color_blob_detection_activity_surface_view);
-        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
-        mOpenCvCameraView.setCvCameraViewListener(this);
+        openCvCameraView = new JavaCameraView(this, -1);
+        setContentView(openCvCameraView);
+        openCvCameraView.setCvCameraViewListener(this);
     }
 
     @Override
-    public void onPause()
-    {
-        super.onPause();
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
-    }
-
-    @Override
-    public void onResume()
-    {
-        super.onResume();
-        if (!OpenCVLoader.initDebug()) {
-            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
-        } else {
-            Log.d(TAG, "OpenCV library found inside package. Using it!");
-            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-        }
-    }
-
-    public void onDestroy() {
-        super.onDestroy();
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
-    }
-
     public void onCameraViewStarted(int width, int height) {
-        mGray = new Mat();
-        mRgba = new Mat();
+        grayscaleImage = new Mat(height, width, CvType.CV_8UC4);
+
+        // The faces will be a 20% of the height of the screen
+        absoluteFaceSize = (int) (height * 0.2);
     }
 
+    @Override
     public void onCameraViewStopped() {
-        mRgba.release();
     }
 
-    public boolean onTouch(View v, MotionEvent event) {
-          return false;
-    }
-
-    public Mat onCameraFrame(CvCameraViewFrame inputFrame) throws FileNotFoundException {
-        mRgba = inputFrame.rgba();
-        mGray = inputFrame.gray();
-
-        if (mAbsoluteFaceSize==0){
-            int height = mGray.rows();
-            if (Math.round(height * mRelativeFaceSize) > 0) {
-                mRelativeFaceSize = Math.round(height * mRelativeFaceSize);
-            }
-        }
+    public Mat onCameraFrame(Mat aInputFrame) {
+        // Create a grayscale image
+        Imgproc.cvtColor(aInputFrame, grayscaleImage, Imgproc.COLOR_RGBA2RGB);
 
         MatOfRect faces = new MatOfRect();
 
-        if (mDetectorType == JAVA_DETECTOR) {
-            if (mJavaDetectorFace != null) {
-                mJavaDetectorFace.detectMultiScale(mGray, faces, 1.1, 2, 2, //TODO: objdetect.CV_HAAR_SCALE_IMAGE)
-                        new Size(mAbsoluteFaceSize, mAbsoluteFaceSize), new Size());
-            }
-        } else {
-            Log.e(TAG, "Detection method is not selected!");
+        // Use the classifier to detect faces
+        if (cascadeClassifier != null) {
+            cascadeClassifier.detectMultiScale(grayscaleImage, faces, 1.1, 2, 2,
+                    new Size(absoluteFaceSize, absoluteFaceSize), new Size());
         }
 
-        Rect[] bananasArray = faces.toArray();
-        for (int i = 0; i < bananasArray.length; i++) {
-            Imgproc.rectangle(mRgba, bananasArray[i].tl(), bananasArray[i].br(), FACE_RECT_COLOR, 3);
-            xCenter = (bananasArray[i].x + bananasArray[i].width + bananasArray[i].x) / 2;
-            yCenter = (bananasArray[i].y + bananasArray[i].height + bananasArray[i].y) / 2;
-        }
+        // If there are any faces found, draw a rectangle around it
+        Rect[] facesArray = faces.toArray();
+        for (Rect rect : facesArray)
+            Imgproc.rectangle(aInputFrame, rect.tl(), rect.br(), new Scalar(0, 255, 0, 255), 3);
 
-
-
-        return mRgba;
+        return aInputFrame;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_3_0, this, mLoaderCallback);
+        } else {
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
 }
