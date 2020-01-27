1e24e90618c7162435f6f49f83e584fe55d2734a
Franz57 <francois.schmidt@outlook.com>
2020-01-27 22:01:15 +0100

    V2

--- app/src/main/java/com/example/opencv/MainActivity.java
+++ app/src/main/java/com/example/opencv/MainActivity.java
@@ -121,4 +121,5 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
             mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
         }
     }
+
 }
